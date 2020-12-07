718b4e4eec0355feebaea69d31f54d7f26d8220d
Alex Boolkin <weronkz@gmail.com>
2020-12-07 14:38:35 +0300

    tag table txt

--- opc2web-client/Program.cs
+++ opc2web-client/Program.cs
@@ -21,9 +21,8 @@ using System.Configuration;
 using OPC.Common;
 using OPC.Data;
 using System.Net;
-using System.Globalization;
 using System.Net.Sockets;
-
+using System.IO;
 
 namespace CSSample
 {
@@ -33,23 +32,27 @@ namespace CSSample
         string serverProgID = ConfigurationManager.AppSettings["opcID"];         // ProgID of OPC server
         private static int timeref = Int32.Parse(ConfigurationManager.AppSettings["refreshTime"]); // OPC refresh time
         private static string consoleOut = ConfigurationManager.AppSettings["consoleOutput"];
-        private static string[] ratios = ConfigurationManager.AppSettings["ratios"].Split(',');
-        private static string[] offsets = ConfigurationManager.AppSettings["offsets"].Split(',');
+
+        static string[] rowsArray = File.ReadAllLines("tags.txt");
+        static string[] tags = new string[rowsArray.Length];
+        static string[] ratios = new string[rowsArray.Length];
+        static string[] offsets = new string[rowsArray.Length];
+        static string[] isbool = new string[rowsArray.Length];
         //web server settings
         private static string webSend = ConfigurationManager.AppSettings["webServer"];
         private static string portNumb = ConfigurationManager.AppSettings["portNumber"];
         // udp settings
-        private static int sendtags = Int32.Parse(ConfigurationManager.AppSettings["tags2send"]); 
-        private static IPAddress remoteIPAddress = IPAddress.Parse(ConfigurationManager.AppSettings["remoteIP"]); 
-        private static int remotePort = Convert.ToInt16(ConfigurationManager.AppSettings["remotePort"]); 
-        private static string udpSend = ConfigurationManager.AppSettings["udpSend"]; 
+        private static int sendtags = Int32.Parse(ConfigurationManager.AppSettings["tags2send"]);
+        private static IPAddress remoteIPAddress = IPAddress.Parse(ConfigurationManager.AppSettings["remoteIP"]);
+        private static int remotePort = Convert.ToInt16(ConfigurationManager.AppSettings["remotePort"]);
+        private static string udpSend = ConfigurationManager.AppSettings["udpSend"];
 
         private OpcServer theSrv;
         private OpcGroup theGrp;
         private static float[] currentValues;
         private static string responseStringG = "";
         private static HttpListener listener = new HttpListener();
-
+        
         public void Work()
         {
             /*	try						// disabled for debugging
@@ -62,7 +65,7 @@ namespace CSSample
             // add our only working group
             theGrp = theSrv.AddGroup("OPCCSharp-Group", false, timeref);
 
-            string[] tags = ConfigurationManager.AppSettings["tags"].Split(','); // tags from config
+           
             if (sendtags > tags.Length) sendtags = tags.Length;
 
             var itemDefs = new OPCItemDef[tags.Length];
@@ -94,7 +97,7 @@ namespace CSSample
             theGrp.Active = true;
             theGrp.DataChanged += new DataChangeEventHandler(this.theGrp_DataChange);
             theGrp.ReadCompleted += new ReadCompleteEventHandler(this.theGrp_ReadComplete);
-            
+
             int CancelID;
 
             int[] aE;
@@ -149,18 +152,39 @@ namespace CSSample
                 {
                     if (consoleOut == "yes")
                     {
-                        Console.WriteLine(" ih={0} v={1} q={2} t={3}", s.HandleClient, s.DataValue, s.Quality, s.TimeStamp); 
+                        Console.WriteLine(" ih={0} v={1} q={2} t={3}", s.HandleClient, s.DataValue, s.Quality, s.TimeStamp);
                     }
-                    currentValues[s.HandleClient] = Convert.ToSingle(s.DataValue) * Single.Parse(ratios[s.HandleClient], CultureInfo.InvariantCulture.NumberFormat)+ Single.Parse(offsets[s.HandleClient], CultureInfo.InvariantCulture.NumberFormat); 
+                    try
+                    {
+                        currentValues[s.HandleClient] = Convert.ToSingle(s.DataValue) * Single.Parse(ratios[s.HandleClient]) + Single.Parse(offsets[s.HandleClient]);
+                    }
+                    catch (FormatException fex) {
+                        Console.WriteLine("Неверный формат числа. Используй запятую вместо точки. {0} ", fex);
+                        File.WriteAllText("error"+ DateTime.Now.ToString("HHmmss") + ".txt", "Неверный формат числа. Используй запятую вместо точки." + "\n " + fex.ToString() + "\n " + fex.Message);
+                        theSrv.Disconnect();
+                    }
                 }
                 else
                     Console.WriteLine(" ih={0}    ERROR=0x{1:x} !", s.HandleClient, s.Error);
             }
             string responseString = "{";
-            for (int i = 0; i < currentValues.Length - 1; i++) responseString = responseString + "\"tag" + i + "\":\"" + currentValues[i] + "\", ";
-            responseString = responseString + "\"tag" + (currentValues.Length - 1) + "\":\"" + currentValues[currentValues.Length - 1] + "\"}";
+            for (int i = 0; i < currentValues.Length - 1; i++)
+            {
+                string value = "";
+                if ((isbool[i] == "b" && currentValues[i] == 1) || (isbool[i] == "!b" && currentValues[i] == 0)) value = "true";
+                else if ((isbool[i] == "b" && currentValues[i] == 0) || (isbool[i] == "!b" && currentValues[i] == 1)) value = "false";
+                else value = currentValues[i].ToString();
+                responseString = responseString + "\"tag" + i + "\":\"" + value + "\", ";
+
+            }
+            string valuelast = "";
+            int lasti = currentValues.Length - 1;
+            if ((isbool[lasti] == "b" && currentValues[lasti] == 1) || (isbool[lasti] == "!b" && currentValues[lasti] == 0)) valuelast = "true";
+            else if ((isbool[lasti] == "b" && currentValues[lasti] == 0) || (isbool[lasti] == "!b" && currentValues[lasti] == 1)) valuelast = "false";
+            else valuelast = currentValues[lasti].ToString();
+            responseString = responseString + "\"tag" + (lasti) + "\":\"" + valuelast + "\"}";
             responseStringG = responseString;
-            
+
             byte[] byteArray = new byte[sendtags * 4];
             Buffer.BlockCopy(currentValues, 0, byteArray, 0, byteArray.Length);
 
@@ -188,12 +212,12 @@ namespace CSSample
         }
         public void theGrp_ReadComplete(object sender, ReadCompleteEventArgs e)
         {
-            Console.WriteLine("ReadComplete event: gh={0} id={1} me={2} mq={3}", e.groupHandleClient, e.transactionID, e.masterError, e.masterQuality);
+            //Console.WriteLine("ReadComplete event: gh={0} id={1} me={2} mq={3}", e.groupHandleClient, e.transactionID, e.masterError, e.masterQuality);
             foreach (OPCItemState s in e.sts)
             {
                 if (HRESULTS.Succeeded(s.Error))
                 {
-                    Console.WriteLine(" ih={0} v={1} q={2} t={3}", s.HandleClient, s.DataValue, s.Quality, s.TimeStamp);
+                    //Console.WriteLine(" ih={0} v={1} q={2} t={3}", s.HandleClient, s.DataValue, s.Quality, s.TimeStamp);
                 }
                 else
                     Console.WriteLine(" ih={0}    ERROR=0x{1:x} !", s.HandleClient, s.Error);
@@ -202,6 +226,25 @@ namespace CSSample
 
         static void Main(string[] args)
         {
+            // считываем вайл tags.txt и добавляем данные в массивы тегов, коэффициентов, оффсетов
+            for (int i = 0; i < rowsArray.Length; i++)
+            {
+                string[] col = rowsArray[i].Split('\t');
+                try
+                {
+                    tags[i] = col[1];
+                    ratios[i] = col[2];
+                    offsets[i] = col[3];
+                    isbool[i] = col[4];
+                    //Console.WriteLine(col[0] + " | " + col[1] + " | " + col[2] + " | " + col[3] + " | " + col[4]);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Несоответствие столбцов, проверь что все столбцы заполнены и разделены табуляцией. {0} ", e);
+                    File.WriteAllText("error" + DateTime.Now.ToString("HHmmss") + ".txt", "Несоответствие столбцов, проверь что все столбцы заполнены и разделены табуляцией." + "\n " + e.ToString() + "\n " + e.Message);
+                }
+                
+            }
             string url = "http://*";
             string port = portNumb;
             string prefix = String.Format("{0}:{1}/", url, port);
@@ -213,7 +256,3 @@ namespace CSSample
         }
     }
 }
-
-/* add this code to app.exe.config file
-
-     */
--- opc2web-client/Properties/AssemblyInfo.cs
+++ opc2web-client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("opc2web")]
+[assembly: AssemblyTitle("opc2web-client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("opc2web")]
+[assembly: AssemblyProduct("opc2web-client")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("d135f1cb-7634-431f-8d3e-d66de7159602")]
+[assembly: Guid("7ae98171-2f68-4a87-a97f-1530849f2b16")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
